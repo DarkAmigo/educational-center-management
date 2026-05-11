14bbb866de146c348bc8069b8bd6c63a84c7616e
DarkAmigo <lucisinandrij331@gmail.com>
2026-05-11 21:47:59 +0300

    Enable Validation via the DRF serialiser

--- lessons/api/serializers.py
+++ lessons/api/serializers.py
@@ -4,14 +4,38 @@ from lessons.models import (
     Lesson,
     Attendance,
 )
+from rest_framework import serializers
+from django.core.exceptions import ValidationError
 
 
-class LessonSerializer(CleanModelSerializer):
+class LessonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Lesson
         fields = "__all__"
 
+    def validate(self, attrs):
+        instance = self.instance
+
+        data = {}
+
+        if instance:
+            for field in Lesson._meta.fields:
+                data[field.name] = getattr(instance, field.name)
+
+        data.update(attrs)
+
+        obj = Lesson(**data)
+
+        if instance:
+            obj.pk = instance.pk
+
+        try:
+            obj.full_clean()
+        except ValidationError as e:
+            raise serializers.ValidationError(e.message_dict)
+
+        return attrs
 
 class AttendanceSerializer(CleanModelSerializer):
 
